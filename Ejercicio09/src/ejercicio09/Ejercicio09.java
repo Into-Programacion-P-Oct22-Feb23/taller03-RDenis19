4fe4788813d76ab5f384c4377bb581f36a7866d8
Denis Ruiz <denisruiz112789@gmail.com>
2022-10-30 10:45:51 -0500

    teller02semana3

--- Ejercicio09/src/ejercicio09/Ejercicio09.java
+++ Ejercicio09/src/ejercicio09/Ejercicio09.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio07;
+package ejercicio09;
 
 /**
  *
  * @author Usuario iTC
  */
-public class Ejercicio07 {
+public class Ejercicio09 {
 
     /**
      * @param args the command line arguments
@@ -16,21 +16,14 @@ public class Ejercicio07 {
     public static void main(String[] args) {
         // TODO code application logic here
         int var1 = 81;
-        int var2 = 9;
-        int var3 = 3;
-        int var4 = 9;
-        int var5 = 10;
-        int var6 = 1;
-        int var7 = 100;
-        int var8 = 25;
-        int var9 = 100;
+        int var2 = 25;
+        int var3 = 10;
+        int var4 = 50;
         
-       boolean val = Math.sqrt(var1) + (var2/var3) == var4) ||; 
-       boolean val = valor (var5 < var6) && ((var7/var8) + Math.sqrt(var9));
-       
-       System.out.println(val);
-       
+        double valor = Math.sqrt(var1) + Math.sqrt(var2) * var3;
+        boolean val = valor >= var4; 
         
+        System.out.println(val);
     }
     
 }
--- Ejercicio10/src/ejercicio10/Ejercicio10.java
+++ Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -2,30 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio06;
+package ejercicio10;
 
 /**
  *
- * @author UTPL
+ * @author Usuario iTC
  */
-public class Ejercicio06 {
+public class Ejercicio10 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int var1 = 81;
-        int var2 = 9;
-        int var3 = 3;
-        int var4 = 9;
-        int var5 = 10;
-        int var6 = 1;
-        
-        double val = (Math.sqrt(var1) + var2/var3 == var4);
-        boolean val = && (var5 > var6);
-        
-        System.out.println(val);
     }
     
 }
